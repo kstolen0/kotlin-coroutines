bf261344b2ee3faac4fb8da89dae6454c45756bc
Kristian Stolen <kstolen0@gmail.com>
2025-06-22 20:21:35 +0800

    yielding control of the thread

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
 package main
 
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
+
 fun main() {
-    println("hello")
+    // create coroutine context
+    runBlocking {
+        // launch a job to run concurrently
+        launch {
+            println("print 1")
+            // suspend job
+            yield()
+            println("print 3")
+        }
+
+        // launch a job to run concurrently
+        launch {
+            println("print 2")
+            // suspend job
+            yield()
+            println("print 4")
+        }
+    }
 }
