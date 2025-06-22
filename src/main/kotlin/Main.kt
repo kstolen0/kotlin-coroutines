89a844325b6ee66523e0ba704eb4623dcf7f3a23
Kristian Stolen <kstolen0@gmail.com>
2025-06-22 14:58:24 +0800

    async

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,34 @@
 package main
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
+
 fun main() {
-    println("hello")
+    // create coroutine context
+    runBlocking {
+        // async is a coroutine builder which returns a Deferred object.
+        // A Deferred object will eventually return a result, or it will fail
+        val taskOne = async {
+            yield()
+            "world"
+        }
+        val taskTwo = async {
+            // uncomment the below line to also cancel the
+            // job awaiting the result from taskTwo
+            // cancel()
+            "hello"
+        }
+
+        launch {
+            // await suspends the current coroutine
+            // until the result is available.
+            // if the job was cancelled then this job will also be cancelled
+            println(taskOne.await())
+        }
+        launch {
+            println(taskTwo.await())
+        }
+    }
 }
