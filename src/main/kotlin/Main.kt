c0b66e639f6068b02a302ae82cf2bbbe6471ac42
Kristian Stolen <kstolen0@gmail.com>
2025-06-22 14:45:11 +0800

    launch job

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,28 @@
 package main
 
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
+
 fun main() {
-    println("hello")
+    runBlocking {
+        // launch a new job so that it can be run
+        // concurrently in the current coroutine scope
+        launch {
+            println("task 1 step 1")
+            // suspend the current job and immediately schedule it
+            // fow when the thread becomes available
+            yield()
+            println("task 1 step 2")
+            yield()
+            println("task 1 step 3")
+        }
+        launch {
+            println("task 2 step 1")
+            yield()
+            println("task 2 step 2")
+            yield()
+            println("task 2 step 3")
+        }
+    }
 }
