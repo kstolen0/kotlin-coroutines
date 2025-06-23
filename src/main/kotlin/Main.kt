a94d7026cb48259cfe711d09f7206431125b256e
Kristian Stolen <kstolen0@gmail.com>
2025-06-23 19:59:48 +0800

    supend until all jobs complete

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,25 @@
 package main
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 fun main() {
-    println("hello")
+    // create a coroutine context
+    runBlocking {
+        // suspend until jobs have completed
+        coroutineScope {
+            launch {
+                delay(100)
+                println("job 1 complete")
+            }
+
+            launch {
+                delay(50)
+                println("job 2 complete")
+            }
+        }
+        println("all jobs completed")
+    }
 }
